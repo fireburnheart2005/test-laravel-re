d99720f5a7845a99d547b72cfb78993816af022c
Tran Khanh <trankhanh.tk.kt@gmail.com>
2014-10-06 21:52:44 +0700

    - Change files permissions - Configure database

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'property_listing',
+			'username'  => 'root',
+			'password'  => 'abcdabcd',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',

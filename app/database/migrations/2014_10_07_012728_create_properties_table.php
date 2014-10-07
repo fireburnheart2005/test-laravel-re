39c776b732174e3521edcd56532d7c03222ea09e
Tran Khanh <trankhanh.tk.kt@gmail.com>
2014-10-07 08:50:56 +0700

    Add properties table migration

---
+++ app/database/migrations/2014_10_07_012728_create_properties_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePropertiesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+	  Schema::create('properties', function($table)
+    {
+      $table->increments('user_id');
+      $table->string('name')->unique();
+      $table->string('description');
+      $table->string('type');
+      $table->string('transaction_type');
+      $table->integer('price');
+      $table->string('price_type');
+      $table->string('area');
+      $table->string('area_type');
+      $table->integer('bedrooms');
+      $table->integer('bathrooms');
+      $table->string('address_number');
+      $table->string('address_street');
+      $table->string('address_ward');
+      $table->string('address_district');
+      $table->string('address_city');
+      $table->string('contact_name');
+      $table->string('contact_mobile');
+      $table->string('contact_telephone');
+      $table->string('contact_note');
+      $table->timestamps();
+    });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('properties');
+	}
+
+}

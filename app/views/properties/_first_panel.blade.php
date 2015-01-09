a16d5b8ee8af37181f5b7537dbf5f4d5be69b81f
Tran <khanhth.evolableasia@gmail.com>
2015-01-09 12:42:33 +0700

    In progress

--- app/views/properties/_first_panel.blade.php
+++ app/views/properties/_first_panel.blade.php
@@ -30,58 +30,63 @@
       <div class="row">
         <div class="col-xs-2 image-box">
           <div class="placeholder">
-            <div class="add-button">
-              Thêm ảnh
+            <div class="add-image-wrapper">
+              <a href="#attachment-modal" class="add-image-link">Thêm ảnh</a>
+              <img src="" alt="" class="img-responsive hidden">
+              <div class="hidden">
+                <input type="file" name="image[]">
+              </div>
             </div>
           </div>
           <div class="text-center"><span class="fa fa-rotate-right fa-lg"></span>&nbsp;&nbsp;<span class="fa fa-trash fa-lg"></span></div>
-          <div class="hidden-file-input">
-            <input class="hidden" type="file" name="image[]"></input>
-          </div>
         </div>
         <div class="col-xs-2 image-box">
           <div class="placeholder">
-            <div class="add-button">
-              Thêm ảnh
+            <div class="add-image-wrapper">
+              <a href="#attachment-modal" class="add-image-link">Thêm ảnh</a>
+              <img src="" alt="" class="img-responsive hidden">
+              <div class="hidden">
+                <input type="file" name="image[]">
+              </div>
             </div>
           </div>
           <div class="text-center"><span class="fa fa-rotate-right fa-lg"></span>&nbsp;&nbsp;<span class="fa fa-trash fa-lg"></span></div>
-          <div class="hidden-file-input">
-            <input class="hidden" type="file" name="image[]"></input>
-          </div>
         </div>
         <div class="col-xs-2 image-box">
           <div class="placeholder">
-            <div class="add-button">
-              Thêm ảnh
+            <div class="add-image-wrapper">
+              <a href="#attachment-modal" class="add-image-link">Thêm ảnh</a>
+              <img src="" alt="" class="img-responsive hidden">
+              <div class="hidden">
+                <input type="file" name="image[]">
+              </div>
             </div>
           </div>
           <div class="text-center"><span class="fa fa-rotate-right fa-lg"></span>&nbsp;&nbsp;<span class="fa fa-trash fa-lg"></span></div>
-          <div class="hidden-file-input">
-            <input class="hidden" type="file" name="image[]"></input>
-          </div>
         </div>
         <div class="col-xs-2 image-box">
           <div class="placeholder">
-            <div class="add-button">
-              Thêm ảnh
+            <div class="add-image-wrapper">
+              <a href="#attachment-modal" class="add-image-link">Thêm ảnh</a>
+              <img src="" alt="" class="img-responsive hidden">
+              <div class="hidden">
+                <input type="file" name="image[]">
+              </div>
             </div>
           </div>
           <div class="text-center"><span class="fa fa-rotate-right fa-lg"></span>&nbsp;&nbsp;<span class="fa fa-trash fa-lg"></span></div>
-          <div class="hidden-file-input">
-            <input class="hidden" type="file" name="image[]"></input>
-          </div>
         </div>
         <div class="col-xs-2 image-box">
           <div class="placeholder">
-            <div class="add-button">
-              Thêm ảnh
+            <div class="add-image-wrapper">
+              <a href="#attachment-modal" class="add-image-link">Thêm ảnh</a>
+              <img src="" alt="" class="img-responsive hidden">
+              <div class="hidden">
+                <input type="file" name="image[]">
+              </div>
             </div>
           </div>
           <div class="text-center"><span class="fa fa-rotate-right fa-lg"></span>&nbsp;&nbsp;<span class="fa fa-trash fa-lg"></span></div>
-          <div class="hidden-file-input">
-            <input class="hidden" type="file" name="image[]"></input>
-          </div>
         </div>
       </div>
 <!--       <div><label>Upload ảnh *</label> <span class="fa fa-cloud-upload fa-2x text-primary"></span></div>
@@ -141,70 +146,72 @@
   </div>
 </div>
 <script type="text/javascript">
-  var imgNum = 0;
-  // trigger upload image
-  $('.fa-cloud-upload').click(function (e) {
-    if (imgNum == 6) {
-      return;
-    }
-    $('input[name="hidden-file-upload-input"]').trigger('click');
-    imgNum++;
-  });
   // read and preview image
-  function readImgURL(input) {
-    var that = input;
-    $clonedEl = $(that).clone(); // clone hidden input
-    $clonedEl.attr('name', 'images[]'); // change the name of cloned el
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
-      reader.onload = function (e) {
-        $el = $('<div class="col-xs-2"><div class="thumbnail"><img class="img-responsive" src="'
-          + e.target.result + '"></div>'
-          + '<div class="text-center"><span class="fa fa-rotate-right fa-lg"></span>&nbsp;&nbsp;<span class="fa fa-trash fa-lg"></span></div>'
-          + '</div>');
-        $('.img-preview').append($el).find('.col-xs-2').append($clonedEl);
-        $(that).val(''); // reset hidden input value
-      }
-      reader.readAsDataURL(input.files[0]);
-    }
-  }
-  // upload image handler
-  $('input[name="hidden-file-upload-input"]').change(function(){
-    readImgURL(this);
-    if (imgNum == 6) {
-      $('.fa-cloud-upload').removeClass('text-primary').addClass('text-danger');
-    }
-  });
-  // trigger update image
-  $('body').on('click', '.fa-rotate-right', function (e) {
-    $(this).parent().parent().find('input').trigger('click');
-    e.preventDefault();
-  });
-  // update image handler
-  $('body').on('change', 'input[name="images[]"]', function (e) {
-    updateImgURL(this);
-  });
-  // read and preview image
-  function updateImgURL(input) {
+  function previewImage(input) {
     var that = input;
     if (input.files && input.files[0]) {
       var reader = new FileReader();
       reader.onload = function (e) {
         var src = e.target.result;
-        $(that).parent().find('img').attr('src', src);
+        // show preview image
+        $(that).parent().parent().find('img').attr('src', src).removeClass('hidden');
+        // hide add image link
+        $(that).parent().parent().find('a.add-image-link').addClass('hidden');
       }
       reader.readAsDataURL(input.files[0]);
     }
   }
-  // delete image
-  $('body').on('click', '.fa-trash', function (e) {
-    $isConfirmed = confirm('Bạn chắc chắn là muốn xóa ảnh?');
-    if ($isConfirmed) {
-      $(this).parent().parent().remove();
-      imgNum--;
-    }
+  // trigger input file click event
+  $('.add-image-wrapper').click(function (e) {
+    $(this).find('.hidden input').trigger('click');
     e.preventDefault();
   });
+  // handle click event
+  $('.add-image-wrapper .hidden input').click(function (e) {
+    e.stopPropagation();
+  }).change(function (e) {
+    previewImage(this);
+  });
+  // read and preview image
+  // function readImgURL(input) {
+  //   var that = input;
+  //   $clonedEl = $(that).clone(); // clone hidden input
+  //   $clonedEl.attr('name', 'images[]'); // change the name of cloned el
+  //   if (input.files && input.files[0]) {
+  //     var reader = new FileReader();
+  //     reader.onload = function (e) {
+  //       $el = $('<div class="col-xs-2"><div class="thumbnail"><img class="img-responsive" src="'
+  //         + e.target.result + '"></div>'
+  //         + '<div class="text-center"><span class="fa fa-rotate-right fa-lg"></span>&nbsp;&nbsp;<span class="fa fa-trash fa-lg"></span></div>'
+  //         + '</div>');
+  //       $('.img-preview').append($el).find('.col-xs-2').append($clonedEl);
+  //       $(that).val(''); // reset hidden input value
+  //     }
+  //     reader.readAsDataURL(input.files[0]);
+  //   }
+  // }
+  // // upload image handler
+  // $('input[name="hidden-file-upload-input"]').change(function(){
+  //   readImgURL(this);
+  //   if (imgNum == 6) {
+  //     $('.fa-cloud-upload').removeClass('text-primary').addClass('text-danger');
+  //   }
+  // });
+  // // trigger update image
+  // $('body').on('click', '.fa-rotate-right', function (e) {
+  //   $(this).parent().parent().find('input').trigger('click');
+  //   e.preventDefault();
+  // });
+  // // update image handler
+  // // delete image
+  // $('body').on('click', '.fa-trash', function (e) {
+  //   $isConfirmed = confirm('Bạn chắc chắn là muốn xóa ảnh?');
+  //   if ($isConfirmed) {
+  //     $(this).parent().parent().remove();
+  //     imgNum--;
+  //   }
+  //   e.preventDefault();
+  // });
 </script>
 <script type="text/javascript">
   $(function (e) {

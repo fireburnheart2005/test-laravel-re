4f45da6e5b7d5d72cd1a6a722acd6469e42834b9
Tran <khanhth.evolableasia@gmail.com>
2015-01-16 11:39:00 +0700

    In progress

--- app/models/Listing.php
+++ app/models/Listing.php
@@ -2,17 +2,33 @@
 
 class Listing extends Eloquent {
 
-	/**
-	* The database table used by the model.
-	*
-	* @var string
-	*/
+    /**
+    * The database table used by the model.
+    *
+    * @var string
+    */
   	protected $table = 'listings';
 
   	protected $guarded = [];
 
+    // Eloquent relationship
     public function images()
     {
         return $this->hasMany('Image');
+    }
+
+    public function city()
+    {
+        return $this->belongsTo('City');
+    }
+
+    public function district()
+    {
+        return $this->belongsTo('District');
+    }
+
+    public function ward()
+    {
+        return $this->belongsTo('Ward');
     }
 }
--- app/views/welcome.blade.php
+++ app/views/welcome.blade.php
@@ -3,8 +3,8 @@
   <div class="row home">
     <div class="col-xs-12">
       @foreach ($listings as $listing)
-        <div class="row">
-          <div class="col-xs-6">
+        <div class="row listing-wrapper">
+          <div class="col-xs-8">
             <div>
               @if (count($listing->images))
                 <img class="pull-left" title="{{$listing->name}}" alt="{{$listing->name}}" src="/assets/{{$listing->images[0]->name}}" width=140 height=110>
@@ -12,12 +12,13 @@
                 <img class="pull-left" title="{{$listing->name}}" alt="{{$listing->name}}" src="/assets/no-image.png" width=140 height=110>
               @endif
               <div class="listing-brief">
-                <h3><a href="/{{$listing->slug}}">{{$listing->name}}</a></h3>
+                <h3><a href="/{{$listing->slug}}" class="text-info">{{$listing->title}}</a></h3>
                 <p>
-                  <span class="text-muted"><span class="fa fa-map-marker"></span> {{$listing->}} | {{$listing->address_city}}</span>
-                  <span class="pull-right text-success">{{$listing->price}} {{$listing->currency}} | {{$listing->area}} {{$listing->area_type}}</span>
+                  <span class="text-muted"><span class="fa fa-map-marker"></span> {{$listing->city->name}} | {{$listing->district->name}}</span>
+                  <span class="pull-right text-info">Giá: {{$listing->price}} | {{$listing->square}} m<sup>2</sup> {{$listing->area_type}}</span>
                 </p>
-                <p>{{StringHelper::truncate(trim(preg_replace('/\s\s+/', ' ',$listing->description)), 100, 150)}} <span class="text-bold text-em">({{date('d/m/Y', strtotime($listing->updated_at))}})</span></p>
+                <p>{{StringHelper::truncate(trim(preg_replace('/\s\s+/', ' ',$listing->description)), 100, 150)}}</p>
+                <p class="text-muted bottom"><span class="fa fa-calendar"></span> ({{date('d/m/Y', strtotime($listing->updated_at))}})</p>
               </div>
             </div>
           </div>
